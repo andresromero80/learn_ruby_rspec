e1508fbe0b0af60b1394b68d0c549459fc5f4ba3
andresromero80 <andresromero80@hotmail.com>
2018-10-08 16:53:55 +0200

    Ajout fichiers

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,49 @@
 # write your code here
+
+def who_is_bigger(a, b, c)
+	
+	if (a == nil || b == nil || c == nil)	
+		ans = "nil detected"
+
+	elsif (a > b && a > c && a)
+			ans = "a is bigger"	
+	
+	elsif  (b > a && b > c)
+			ans = "b is bigger"	
+		
+	elsif  (c > a && c > b)
+			ans = "c is bigger"		
+	end
+
+
+	ans
+end 
+
+
+def reverse_upcase_noLTA (string)
+
+	reversed = string.reverse
+	upcased = reversed.upcase
+	noL = upcased.delete("L")
+	noLT = noL.delete("T")
+	noLTA = noLT.delete("A")
+
+
+end 
+
+def array_42 (array)
+	array.include? 42
+end 
+
+def magic_array(array)
+	array.flatten.sort.collect{|n|n*2 if n%3!=0}.compact.uniq
+end 
+
+
+# def magic_array(array)
+# 	array.flatten.sort.collect { |n| 
+# 		if n % 3 != 0
+# 			n * 2 
+# 		end 
+# 	}.compact.uniq
+# end 
--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
 #write your code here
+def echo(string)
+	"#{string}"
+end 
+
+def shout(string)
+	"#{string.upcase}"
+end 
+
+def repeat (string,n=2)
+#	puts "#{string} #{string}" 
+	line = String.new
+	n.times do |i|
+		# if i != n-1
+		line += "#{string} "
+	end 
+	line.strip
+end 
+
+def start_of_word(string, int)
+	string[0,int]
+end 
+
+def first_word(string)
+	
+
+end 
