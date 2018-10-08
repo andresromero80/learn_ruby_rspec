7793f98fc253f0354d4ba7f3ae76903dbfe24ac8
andresromero80 <andresromero80@hotmail.com>
2018-10-08 19:56:37 +0200

    Ajout timer

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,20 @@
 #write your code here
+def time_string(t)
+
+	min, sec = t.divmod(60)            #=> [min, sec]
+	hour, min = min.divmod(60)           #=> [hour, min]
+	
+	if sec < 10
+		sec = "0#{sec}"
+	end
+
+	if min < 10
+		min = "0#{min}"
+	end
+
+	if hour < 10
+		hour = "0#{hour}"
+	end
+	
+	return "#{hour}:#{min}:#{sec}"
+end 
